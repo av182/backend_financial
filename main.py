a69c28a74c4e05fc5eb3b0704a44e274906ecf65
User <me@example.com>
2026-04-10 16:33:23 +0300

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import uvicorn
 from fastapi import FastAPI
-import requests
 import json
 
 app = FastAPI(title='Finance Application', version='1.0.0')
@@ -10,6 +9,9 @@ app = FastAPI(title='Finance Application', version='1.0.0')
 async def index():
     return {'msg': "Hello, world!"}
    
+@app.get("/{bank_id}")
+async def index(bank_id: int):
+    return {'msg': f"Hello, bank with id {bank_id}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=9011, reload = True) 
